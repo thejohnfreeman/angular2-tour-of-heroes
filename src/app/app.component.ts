6a4b98187c8bf3f26eb9e9df6ce316ffa3d87362
John Freeman <jfreeman08@gmail.com>
2017-02-12 18:57:18 -0500

    Add Hero selection
    https://angular.io/docs/ts/latest/tutorial/toh-pt2.html#!#selecting-a-hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -75,24 +75,27 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
 
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
     </div>
     `,
 })
 export class AppComponent {
   public title = "Tour of Heroes";
   public heroes = HEROES;
-  public hero: Hero = {
-    id: 1,
-    name: "Windstorm",
-  };
+  public selectedHero: Hero;
+
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
