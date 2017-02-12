8e92cfb0590391b890dee015ecab676b19a7c0cc
John Freeman <jfreeman08@gmail.com>
2017-02-12 18:46:50 -0500

    Add list of Heroes
    https://angular.io/docs/ts/latest/tutorial/toh-pt2.html#!#displaying-our-heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,30 @@ export class Hero {
   public name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: "Mr. Nice" },
+  { id: 12, name: "Narco" },
+  { id: 13, name: "Bombasto" },
+  { id: 14, name: "Celeritas" },
+  { id: 15, name: "Magneta" },
+  { id: 16, name: "RubberMan" },
+  { id: 17, name: "Dynama" },
+  { id: 18, name: "Dr IQ" },
+  { id: 19, name: "Magma" },
+  { id: 20, name: "Tornado" },
+];
+
 @Component({
   selector: "app-element",
   template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
@@ -21,6 +41,7 @@ export class Hero {
 })
 export class AppComponent {
   public title = "Tour of Heroes";
+  public heroes = HEROES;
   public hero: Hero = {
     id: 1,
     name: "Windstorm",
