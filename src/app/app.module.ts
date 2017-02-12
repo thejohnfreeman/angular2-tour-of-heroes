65c425524fa4432149a4daa8d0b5eab748f2846c
John Freeman <jfreeman08@gmail.com>
2017-02-12 18:38:56 -0500

    Use two-way binding
    https://angular.io/docs/ts/latest/tutorial/toh-pt1.html#!#two-way-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
@@ -6,6 +7,6 @@ import { AppComponent } from "./app.component";
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
