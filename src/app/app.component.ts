37bc2d302cf46ada65fe0e5bdf4f5645e44acd99
John Freeman <jfreeman08@gmail.com>
2017-02-12 18:31:58 -0500

    Add Hero class
    https://angular.io/docs/ts/latest/tutorial/toh-pt1.html#!#hero-object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+// tslint:disable:max-classes-per-file
+
 import { Component } from "@angular/core";
+
+export class Hero {
+  public id: number;
+  public name: string;
+}
 
 @Component({
   selector: "app-element",
-  template: "<h1>{{title}}</h1><h2>{{hero}} details!</h2>",
+  template: "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>",
 })
 export class AppComponent {
   public title = "Tour of Heroes";
-  public hero = "Windstorm";
+  public hero: Hero = {
+    id: 1,
+    name: "Windstorm",
+  };
 }
