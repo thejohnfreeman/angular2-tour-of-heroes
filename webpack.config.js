01e69d480dfd95895151aa77235728d0f5bc74b6
John Freeman <jfreeman08@gmail.com>
2017-02-12 16:19:09 -0500

    Switch from JavaScript to TypeScript
    We configure Webpack to load and compile TypeScript:
    
    `$ yarn add typescript ts-loader --dev`
    
    https://webpack.js.org/guides/webpack-and-typescript/
    https://www.typescriptlang.org/docs/handbook/integrating-with-build-tools.html#webpack
    
    We rename our JavaScript source file, which is valid TypeScript, to use the
    TypeScript file extension `.ts`.
    
    We configure the TypeScript compiler to prepare for Angular. Thankfully,
    `tsconfig.json` allows comments!
    
    https://angular.io/docs/ts/latest/guide/typescript-configuration.html
    https://webpack.js.org/guides/webpack-and-typescript/
    https://www.typescriptlang.org/docs/handbook/compiler-options.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 var path = require('path')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   devServer: {
     // Search for static files under this path.
