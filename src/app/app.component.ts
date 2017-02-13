f3109bf920e7709b08508035d800d77cc3d28ef6
John Freeman <jfreeman08@gmail.com>
2017-02-12 20:24:24 -0500

    Factor HeroService
    https://angular.io/docs/ts/latest/tutorial/toh-pt4.html#!#creating-a-hero-service
    
    We create a Service and use it with dependency injection. For that, we
    decorate it with `@Injectable`, which requires an additional feature from
    TypeScript.
    
    https://angular.io/docs/ts/latest/guide/typescript-configuration.html
    http://stackoverflow.com/a/42015303/618906
    https://blog.thoughtram.io/angular/2015/09/17/resolve-service-dependencies-in-angular-2.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
 // tslint:disable:max-classes-per-file
 
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Hero } from "./hero";
-
-const HEROES: Hero[] = [
-  { id: 11, name: "Mr. Nice" },
-  { id: 12, name: "Narco" },
-  { id: 13, name: "Bombasto" },
-  { id: 14, name: "Celeritas" },
-  { id: 15, name: "Magneta" },
-  { id: 16, name: "RubberMan" },
-  { id: 17, name: "Dynama" },
-  { id: 18, name: "Dr IQ" },
-  { id: 19, name: "Magma" },
-  { id: 20, name: "Tornado" },
-];
+import { HeroService } from "./hero.service";
 
 @Component({
+  providers: [HeroService],
   selector: "app-element",
   styles: [`
     .selected {
@@ -81,10 +70,20 @@ const HEROES: Hero[] = [
     <hero-detail [hero]="selectedHero"></hero-detail>
     `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private title = "Tour of Heroes";
-  private heroes = HEROES;
+  private heroes: Hero[];
   private selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  public ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  private getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   private onSelect(hero: Hero): void {
     this.selectedHero = hero;
---
+++ src/app/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+
+@Injectable()
+export class HeroService {
+  public getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
