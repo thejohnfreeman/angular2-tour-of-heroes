6d4f9e49784eae5b2efc306c157b52e1d11a47da
John Freeman <jfreeman08@gmail.com>
2017-02-13 00:42:48 -0500

    Add first route, to HeroesComponent
    https://angular.io/docs/ts/latest/tutorial/toh-pt5.html#!#add-routing
    
    We add the Angular Router.
    
    `$ yarn add @angular/router`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from "@angular/core";
   selector: "app-element",
   template: `
     <h1>{{title}}</h1>
-    <heroes></heroes>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
     `,
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HeroDetailComponent } from "./hero-detail.component";
@@ -10,7 +11,16 @@ import { HeroesComponent } from "./heroes.component";
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        component: HeroesComponent,
+        path: "heroes",
+      },
+    ]),
+  ],
   providers: [HeroService],
 })
 export class AppModule {}
