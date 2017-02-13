9bb66768a775da72716108913045dfaa4f51c39e
John Freeman <jfreeman08@gmail.com>
2017-02-13 00:49:02 -0500

    Add route to DashboardComponent
    https://angular.io/docs/ts/latest/tutorial/toh-pt5.html#!#add-a-dashboard-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,35 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard.component";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroService } from "./hero.service";
 import { HeroesComponent } from "./heroes.component";
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "/dashboard",
+      },
+      {
         component: HeroesComponent,
         path: "heroes",
+      },
+      {
+        component: DashboardComponent,
+        path: "dashboard",
       },
     ]),
   ],
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "dashboard",
+  template: "<h3>Dashboard</h3>",
+})
+export class DashboardComponent {
+}
