aceb0a09dc3e1362e82991847ddb554ebebc71d8
John Freeman <jfreeman08@gmail.com>
2017-02-13 02:06:58 -0500

    Factor template for HeroDetailComponent

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -9,17 +9,7 @@ import { HeroService } from "./hero.service";
 
 @Component({
   selector: "hero-detail",
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-      <button (click)="goBack()">Back</button>
-    </div>
-  `,
+  templateUrl: "./hero-detail.component.html",
 })
 export class HeroDetailComponent implements OnInit {
   @Input()
