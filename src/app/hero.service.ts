483eef9085521c66132bd8f79174722fad87f36f
John Freeman <jfreeman08@gmail.com>
2017-02-12 20:32:43 -0500

    Add HeroService.getHeroesSlowly
    https://angular.io/docs/ts/latest/tutorial/toh-pt4.html#!#appendix-take-it-slow

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,4 +8,10 @@ export class HeroService {
   public getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+
+  public getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
 }
