4e879cbd125fd50340d8982b2fd0e9aa30ead756
John Freeman <jfreeman08@gmail.com>
2017-02-13 21:32:12 -0500

    Add Hero detail style
    https://angular.io/docs/ts/latest/tutorial/toh-pt5.html#!#stylish-hero-details

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Location } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 
 import "rxjs/add/operator/switchMap";
@@ -9,10 +9,10 @@ import { HeroService } from "./hero.service";
 
 @Component({
   selector: "hero-detail",
+  styleUrls: ["./hero-detail.component.css"],
   templateUrl: "./hero-detail.component.html",
 })
 export class HeroDetailComponent implements OnInit {
-  @Input()
   private hero: Hero;
 
   constructor(
