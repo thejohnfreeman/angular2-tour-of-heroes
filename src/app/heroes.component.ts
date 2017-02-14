3f9fdb4fddbfb015e94d9220979132d5fa7f0967
John Freeman <jfreeman08@gmail.com>
2017-02-13 21:24:20 -0500

    Implement HeroesComponent.gotoDetail with Router
    https://angular.io/docs/ts/latest/tutorial/toh-pt5.html#!#update-the-_heroescomponent_-class-

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 import { Hero } from "./hero";
 import { HeroService } from "./hero.service";
@@ -12,7 +13,10 @@ export class HeroesComponent implements OnInit {
   private heroes: Hero[];
   private selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private router: Router,
+    private heroService: HeroService,
+  ) {}
 
   public ngOnInit(): void {
     this.getHeroes();
@@ -24,5 +28,9 @@ export class HeroesComponent implements OnInit {
 
   private onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  private gotoDetail(): void {
+    this.router.navigate(["/detail", this.selectedHero.id]);
   }
 }
