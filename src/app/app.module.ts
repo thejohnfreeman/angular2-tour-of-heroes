031d4761b75ed13fd73e5965af9adec4e20f9b9a
John Freeman <jfreeman08@gmail.com>
2017-02-13 20:45:17 -0500

    Factor AppRoutingModule
    https://angular.io/docs/ts/latest/tutorial/toh-pt5.html#!#refactor-routes-to-a-_routing-module_

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { DashboardComponent } from "./dashboard.component";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroService } from "./hero.service";
 import { HeroesComponent } from "./heroes.component";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -20,25 +21,7 @@ import { HeroesComponent } from "./heroes.component";
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: "",
-        pathMatch: "full",
-        redirectTo: "/dashboard",
-      },
-      {
-        component: DashboardComponent,
-        path: "dashboard",
-      },
-      {
-        component: HeroDetailComponent,
-        path: "detail/:id",
-      },
-      {
-        component: HeroesComponent,
-        path: "heroes",
-      },
-    ]),
+    AppRoutingModule,
   ],
   providers: [HeroService],
 })
